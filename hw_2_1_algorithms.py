821a3ab10a615cff0c063abe6688c3769112a7b1
Vlad <zubun7@gmail.com>
2021-10-23 15:35:46 +0300

    22

--- hw_2_1_algorithms.py
+++ hw_2_1_algorithms.py
@@ -1,38 +1,3 @@
-# def sum_sum():
-#     stop = ("w")
-#     number_summ_all = 0
-#     while stop != ("q"):
-#         number_summ = []
-#         line = input("введите строку: ")
-#         number_str = []
-#         number_str = line.split()
-#         st = 0
-#         st = number_str.count("q")
-#         if st >0:
-#             # number_str.index("q")
-#             number_int = list(map(int, number_str[0: number_str.index("q")]))
-#             # number_str.remove("q")
-#             # number_int = list(map(int, number_str))
-#             number_summ = sum(number_int)
-#             number_summ_all += number_summ
-#             return number_summ_all
-#         number_int = list(map(int,number_str))
-#         number_summ = sum(number_int)
-#         number_summ_all += number_summ
-#         stop = input(f"Сумма всех значений равна: {number_summ_all}. Если желаете продолжить введите w, для завршения введите q:")
-#     return number_summ_all
-#
-#
-# summ = sum_sum()
-# print(f"Сумма введеных значений равна: {summ}.")
-#
-# a = input('Введите трехзначное число')
-# match list(a):
-#     case a1, a2, a3:
-#         print(int(a1) + int(a2) + int(a3))
-#         print(int(a1) * int(a2) * int(a3))
-#     case _:
-#         print('Вы ввели неверное значение.')
 
 #1. Написать программу, которая будет складывать, вычитать, умножать или делить 0 числа. Числа и знак операции вводятся пользователем.
 # После выполнения вычисления программа не должна завершаться, а должна запрашивать новые данные для вычислений.
